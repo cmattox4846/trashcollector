1ce14df495cb91d4ffdac056dcb56b01226411b5
cmattox4846 <cmattox4846@gmail.com>
2021-10-25 15:41:50 -0400

    added employee create

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=5)
-    
+    badge_number = models.IntegerField()
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,10 +1,29 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.apps import apps
+from datetime import date
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
+from .models import Employee
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
+
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        badge_number_from_form = request.POST.get('badge_number')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, zip_code=zip_from_form, badge_number=badge_number_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
 
 # create qurey and filter to diplay customers for the day
 
@@ -14,6 +33,23 @@ from django.apps import apps
 
 
 def index(request):
+
+    # The following line will get the logged-in user (if there is one) within any view function
+    logged_in_user = request.user
+    try:
+        # This line will return the employee record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+        
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # Customer = apps.get_model('customers.Customer')
+    # return render(request, 'employees/index.html')
